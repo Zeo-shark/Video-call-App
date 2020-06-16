b8d37dd64c24b412de721ef6d0de2d98e68eb271
saurav <sauravberaiit@gmail.com>
2020-06-16 17:17:22 +0530

    Added simple Agora Flutter Template to watsapp

---
+++ android/app/src/main/kotlin/com/example/watsapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.watsapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/utils/settings.dart
@@ -0,0 +1,2 @@
+// Agora AppId
+const APP_ID = '1c2a970195c14939be4dd8649659dce5';
